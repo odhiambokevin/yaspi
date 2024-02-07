8d458e45fa981192fe0170ac49dd4bdf6494d0ac
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2024-02-07 14:41:41 +0300

    refactor: remove geodjango

--- backend/yaspi/settings/base.py
+++ backend/yaspi/settings/base.py
@@ -17,7 +17,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
+    # 'django.contrib.gis',
 ]
 
 THIRD_PARTY_APPS = [    
