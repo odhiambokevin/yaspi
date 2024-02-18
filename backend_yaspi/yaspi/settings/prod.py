1202aa1198b3a0c15fcc098fc1cbd0f4ad207970
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2024-02-18 17:34:06 +0300

    refactor: edit allowed cors origin

--- backend_yaspi/yaspi/settings/prod.py
+++ backend_yaspi/yaspi/settings/prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['yaspi-be.vercel.app']
 
-CORS_ALLOWED_ORIGINS = ["yaspikenya.com", "https://portfolio-sigma-two-34.vercel.app"]
+CORS_ALLOWED_ORIGINS = ["yaspikenya.com", "yaspi.vercel.app"]
 CORS_ALLOWED_METHODS = ["DELETE","GET","OPTIONS","PATCH","POST","PUT",]
 
 # HTTPS SETTINGS
